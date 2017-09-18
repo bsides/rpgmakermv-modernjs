593e41fd821062c08144569fc71009e257c7c6fc
Rafael BSIDES Pereira <rafaelbsides@gmail.com>
2017-09-18 11:06:45 -0300

    Preparing for Utils class

--- src/core/rpg_core.js
+++ src/core/rpg_core.js
@@ -1,7 +1,11 @@
 import { Bitmap } from './bitmap'
+import { Utils } from './utils'
 import { CacheEntry, CacheMap, ImageCache, RequestQueue } from './cache'
 
 window.Bitmap = Bitmap
+
+window.Utils = Utils
+
 window.CacheEntry = CacheEntry
 window.CacheMap = CacheMap
 window.ImageCache = ImageCache
